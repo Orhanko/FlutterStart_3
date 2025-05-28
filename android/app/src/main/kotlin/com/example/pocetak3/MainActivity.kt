9bd8d3d4cf054f3bd1281c4fbfa9e4d7106068fc
Orhan Pojskic <ohran.pojskic.20@size.ba>
2025-05-28 14:22:33 +0200

    Created repository for new flutter mini project

---
+++ android/app/src/main/kotlin/com/example/pocetak3/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.pocetak3
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:pocetak3/expenses.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Column(children: [Text('Chart placeholder...'), Expenses()]),
+    ),
+  );
+}
