20f1c5dafb476242d9ba27ff36e34a9ff79fb497
Orhan Pojskic <ohran.pojskic.20@size.ba>
2025-06-02 21:00:01 +0200

    Fixed the device orientation on portrait mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pocetak3/expenses.dart';
+import 'package:flutter/services.dart';
 
 var kColorScheme = ColorScheme.fromSeed(seedColor: Colors.amber);
 var kDarkColorScheme = ColorScheme.fromSeed(
@@ -8,51 +9,56 @@ var kDarkColorScheme = ColorScheme.fromSeed(
 );
 
 void main() {
-  runApp(
-    MaterialApp(
-      darkTheme: ThemeData.dark().copyWith(
-        colorScheme: kDarkColorScheme,
-        appBarTheme: AppBarTheme().copyWith(
-          backgroundColor: kDarkColorScheme.inversePrimary,
-        ),
-        cardTheme: CardTheme().copyWith(
-          color: kDarkColorScheme.inversePrimary,
-          margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-        ),
-        textTheme: ThemeData().textTheme.copyWith(
-          titleLarge: TextStyle(
-            fontWeight: FontWeight.bold,
-            color: kDarkColorScheme.onTertiaryContainer,
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then((
+    fn,
+  ) {
+    runApp(
+      MaterialApp(
+        darkTheme: ThemeData.dark().copyWith(
+          colorScheme: kDarkColorScheme,
+          appBarTheme: AppBarTheme().copyWith(
+            backgroundColor: kDarkColorScheme.inversePrimary,
           ),
-          bodyLarge: TextStyle(color: Colors.white),
+          cardTheme: CardTheme().copyWith(
+            color: kDarkColorScheme.inversePrimary,
+            margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          ),
+          textTheme: ThemeData().textTheme.copyWith(
+            titleLarge: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: kDarkColorScheme.onTertiaryContainer,
+            ),
+            bodyLarge: TextStyle(color: Colors.white),
+          ),
         ),
+        theme: ThemeData().copyWith(
+          colorScheme: kColorScheme,
+          appBarTheme: AppBarTheme().copyWith(
+            backgroundColor: kColorScheme.onPrimaryFixedVariant,
+            foregroundColor: kColorScheme.primaryContainer,
+          ),
+          cardTheme: CardTheme().copyWith(
+            color: kColorScheme.inversePrimary,
+            margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: kColorScheme.primaryContainer,
+            ),
+          ),
+          textTheme: ThemeData().textTheme.copyWith(
+            titleLarge: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: kColorScheme.onTertiaryContainer,
+            ),
+            bodyLarge: TextStyle(color: Colors.black),
+          ),
+          scaffoldBackgroundColor: kColorScheme.secondaryFixedDim,
+        ),
+        themeMode: ThemeMode.system,
+        home: Expenses(),
       ),
-      theme: ThemeData().copyWith(
-        colorScheme: kColorScheme,
-        appBarTheme: AppBarTheme().copyWith(
-          backgroundColor: kColorScheme.onPrimaryFixedVariant,
-          foregroundColor: kColorScheme.primaryContainer,
-        ),
-        cardTheme: CardTheme().copyWith(
-          color: kColorScheme.inversePrimary,
-          margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-        ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: kColorScheme.primaryContainer,
-          ),
-        ),
-        textTheme: ThemeData().textTheme.copyWith(
-          titleLarge: TextStyle(
-            fontWeight: FontWeight.bold,
-            color: kColorScheme.onTertiaryContainer,
-          ),
-          bodyLarge: TextStyle(color: Colors.black),
-        ),
-        scaffoldBackgroundColor: kColorScheme.secondaryFixedDim,
-      ),
-      themeMode: ThemeMode.system,
-      home: Expenses(),
-    ),
-  );
+    );
+  });
 }
